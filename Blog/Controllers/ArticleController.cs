d24b93d2970a7ff5dbf82f2b9c5d5dfa8ffbad19
PlamenHP <p.petrov.mail@gmail.com>
2016-12-05 23:51:53 +0000

    added Get and Post method for article create

--- Blog/Controllers/ArticleController.cs
+++ Blog/Controllers/ArticleController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -27,6 +28,31 @@ namespace Blog.Controllers
                     .ToList();
 
                 return View(articles);
+            }
+        }
+
+        // GET: Article/Detail
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                // Get the article from database
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (article == null )
+                {
+                    return HttpNotFound();
+                }
+
+                return View(article);
             }
         }
     }
