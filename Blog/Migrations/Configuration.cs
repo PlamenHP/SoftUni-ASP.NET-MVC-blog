2c2b29934b7355d8f6b5d007f7d200b609ed33f7
PlamenHP <p.petrov.mail@gmail.com>
2016-12-07 00:26:54 +0000

    added: automatic creation of default roles and admin account in the DB

--- Blog/Migrations/Configuration.cs
+++ Blog/Migrations/Configuration.cs
@@ -1,7 +1,9 @@
 namespace Blog.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
     using System;
-    using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
 
@@ -14,20 +16,88 @@ namespace Blog.Migrations
             AutomaticMigrationDataLossAllowed = true;
         }
 
-        protected override void Seed(Blog.Models.BlogDbContext context)
+        protected override void Seed(BlogDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
+            if (!context.Roles.Any())
+            {
+                this.CreateRole(context, "Admin");
+                this.CreateRole(context, "User");
+            }
 
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            if (!context.Roles.Any(r=>r.Name == "Admin"))
+            {
+                this.CreateRole(context, "Admin");
+            }
+
+            if (!context.Roles.Any(r => r.Name == "User"))
+            {
+                this.CreateRole(context, "User");
+            }
+
+            if (!context.Users.Any())
+            {
+                this.CreateUser(context, "admin@admin.com", "Admin", "123");
+                this.SetRoleToUser(context, "admin@admin.com", "Admin");
+            }
+        }
+
+        private void CreateRole(BlogDbContext context, string roleName)
+        {
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+
+            var result = roleManager.Create(new IdentityRole(roleName));
+
+            if (!result.Succeeded)
+            {
+                throw new Exception(string.Join(";", result.Errors));
+            }
+        }
+
+        private void CreateUser(BlogDbContext context, string email, string fullName, string password)
+        {
+            // Create user manager
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            // Set user manager password validator
+            userManager.PasswordValidator = new PasswordValidator
+            {
+                RequiredLength = 1,
+                RequireDigit = false,
+                RequireLowercase = false,
+                RequireNonLetterOrDigit = false,
+                RequireUppercase = false,
+            };
+
+            // Create user object
+            var admin = new ApplicationUser
+            {
+                UserName = email,
+                FullName = fullName,
+                Email = email,
+            };
+
+            // Create user
+            var result = userManager.Create(admin, password);
+
+            // Validate result
+            if (!result.Succeeded)
+            {
+                throw new Exception(string.Join(";",result.Errors));
+            }
+        }
+
+        private void SetRoleToUser(BlogDbContext context , string email, string role)
+        {
+            var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            var user = context.Users.Where(u=>u.Email == email).First();
+
+            var result = userManager.AddToRole(user.Id, role);
+
+            if(!result.Succeeded)
+            {
+                throw new Exception(string.Join(";",result.Errors));
+            }
         }
     }
 }
