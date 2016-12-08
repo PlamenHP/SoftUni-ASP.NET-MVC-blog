df432c0f07b7ee556e8b6216654805a606bf4ca0
PlamenHP <p.petrov.mail@gmail.com>
2016-12-08 00:45:38 +0000

    added: editing users

--- Blog/Controllers/Admin/UserController.cs
+++ Blog/Controllers/Admin/UserController.cs
@@ -3,7 +3,12 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web.Mvc;
+using System;
+using System.Web;
+using Microsoft.AspNet.Identity.Owin;
+using System.Data.Entity;
 
 namespace Blog.Controllers.Admin
 {
@@ -23,6 +28,7 @@ namespace Blog.Controllers.Admin
                 var users = database.Users.ToList();
 
                 var admins = GetAdminUserNames(users, database);
+                ViewBag.Admins = admins;
 
                 return View(users);
             }
@@ -44,5 +50,129 @@ namespace Blog.Controllers.Admin
 
             return admins;
         }
+
+
+        // GET: User/Edit
+        [Authorize]
+        public ActionResult Edit(string id)
+        {
+            //validate id
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                // Get user from DB
+                var user = database.Users.FirstOrDefault(u => u.Id == id);
+
+                // Check if user exists
+                if (user == null)
+                {
+                    return HttpNotFound();
+                }
+
+
+                // Create a view model
+                var viewModel = new EditUserViewModel();
+                viewModel.User = user;
+                viewModel.Roles = GetUserRoles(user, database);
+
+                // Pass the model to the view
+                return View(viewModel);
+            }
+        }
+
+        // POST: User/Edit
+        [HttpPost]
+        public ActionResult Edit(string id, EditUserViewModel viewModel)
+        {
+            // Check if model is valid
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    // Get user from database
+                    var user = database.Users.FirstOrDefault(u=>u.Id == id);
+
+                    // Check if user exists
+                    if (user == null)
+                    {
+                        return HttpNotFound();
+                    }
+
+                    // If password field is not empty, change password
+                    if (!string.IsNullOrEmpty(viewModel.Password))
+                    {
+                        var hasher = new PasswordHasher();
+                        var passwordHash = hasher.HashPassword(viewModel.Password);
+                        user.PasswordHash = passwordHash;
+                    }
+
+                    // Set user properties
+                    user.Email = viewModel.User.Email;
+                    user.FullName = viewModel.User.FullName;
+                    user.UserName = viewModel.User.UserName;
+                    this.SetUserRoles(viewModel, user, database);
+
+                    // Save changes
+                    database.Entry(user).State = EntityState.Modified;
+                    return RedirectToAction("List"); 
+                }
+            }
+
+            return View(viewModel);
+        }
+
+        private void SetUserRoles(EditUserViewModel viewModel, BlogUser user, BlogDbContext database)
+        {
+            var userManager = HttpContext.GetOwinContext()
+                                        .GetUserManager<ApplicationUserManager>();
+
+            foreach (var role in viewModel.Roles)
+            {
+                if (role.IsSelected && !userManager.IsInRole(user.Id, role.Name))
+                {
+                    userManager.AddToRole(user.Id, role.Name);
+                }
+                else if (!role.IsSelected && userManager.IsInRole(user.Id, role.Name))
+                {
+                    userManager.RemoveFromRole(user.Id, role.Name);
+                }
+            }
+        }
+
+        private IList<Role> GetUserRoles(BlogUser user, BlogDbContext db)
+        {
+            // Create user manager
+            var userManager = Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+            // Get all  application roles
+            var roles = db.Roles.
+                Select(r => r.Name)
+                .OrderBy(r => r)
+                .ToList();
+
+            // For each application role, check if the user gas it
+            var userRoles = new List<Role>();
+
+            foreach (var roleName in roles)
+            {
+                var role = new Role { Name = roleName };
+
+                if (userManager.IsInRole(user.Id, roleName))
+                {
+                    role.IsSelected = true;
+                }
+
+                userRoles.Add(role);
+            }
+
+            // Return a list with all roles
+            return userRoles;
+        }
+
+
     }
 }
